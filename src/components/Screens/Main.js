10c4baa78552ed03fdc57e2bef1aa8d7b18ca907
Gabriel Brolo <gabrolo13@gmail.com>
2018-09-21 21:53:09 -0600

    Equality operation

--- src/components/Screens/Main.js
+++ src/components/Screens/Main.js
@@ -36,40 +36,48 @@ class Main extends Component {
     }
 
     handleOperation(currentOperation) {
-        if (!this.state.currentOperation && this.state.lastOperation === '') {
-            this.setState({ currentOperation });
-        } else if (this.state.lastOperation === '+') {
-            var newNumber = (parseInt(this.state.display) + parseInt(this.state.secondNumber)).toString();
-            var display = this.validateMaxNumber(newNumber) ? 'ERROR' : newNumber;
-            
-            if (display != 'ERROR') {
-                display = this.validateLength(display) ? display : display.substring(0, 8);
+        if (this.state.currentOperation === '=') {
+            this.setState({ currentOperation, lastOperation: '' });
+        } else {
+            if (!this.state.currentOperation && this.state.lastOperation === '') {
+                this.setState({ currentOperation });
+            } else if (this.state.lastOperation === '+') {
+                var newNumber = (parseInt(this.state.display) + parseInt(this.state.secondNumber)).toString();
+                var display = this.validateMaxNumber(newNumber) ? 'ERROR' : newNumber;
+                
+                if (display != 'ERROR') {
+                    display = this.validateLength(display) ? display : display.substring(0, 8);
+                }
+                
+                this.setState({ display, secondNumber: '', currentOperation, lastOperation: '' });
+            } else if (this.state.lastOperation === '-') {
+                var newNumber = (parseInt(this.state.secondNumber) - parseInt(this.state.display)).toString();
+                var display = this.validateMaxNumber(newNumber) ? 'ERROR' : newNumber;
+                
+                if (display != 'ERROR') {
+                    display = this.validateLength(display) ? display : display.substring(0, 8);
+                }
+                
+                this.setState({ display, secondNumber: '', currentOperation, lastOperation: '' });
             }
-            
-            this.setState({ display, secondNumber: '', currentOperation, lastOperation: '' });
-        } else if (this.state.lastOperation === '-') {
-            var newNumber = (parseInt(this.state.secondNumber) - parseInt(this.state.display)).toString();
-            var display = this.validateMaxNumber(newNumber) ? 'ERROR' : newNumber;
-            
-            if (display != 'ERROR') {
-                display = this.validateLength(display) ? display : display.substring(0, 8);
-            }
-            
-            this.setState({ display, secondNumber: '', currentOperation, lastOperation: '' });
-        }
+        }        
     }
 
     handleDigit(digit) {
-        if (!this.state.currentOperation ) {
-            var currentDisplay = this.state.display === '0' ? '' : this.state.display;
-            var newNumber = currentDisplay + digit;
-            newNumber = newNumber === '' ? '0' : newNumber;
-            var display = this.validateLength(newNumber) ? newNumber : this.state.display;
-            display = this.validateMaxNumber(display) ? 'ERROR' : display;
-            this.setState({ display, currentOperation: null });
-        } else if (this.state.currentOperation) {
-            this.setState({ secondNumber: this.state.display, display : digit, currentOperation: null, lastOperation: this.state.currentOperation });
-        }
+        if (this.state.currentOperation != '=') {
+            if (!this.state.currentOperation ) {
+                var currentDisplay = this.state.display === '0' ? '' : this.state.display;
+                var newNumber = currentDisplay + digit;
+                newNumber = newNumber === '' ? '0' : newNumber;
+                var display = this.validateLength(newNumber) ? newNumber : this.state.display;
+                display = this.validateMaxNumber(display) ? 'ERROR' : display;
+                this.setState({ display, currentOperation: null });
+            } else if (this.state.currentOperation) {
+                this.setState({ secondNumber: this.state.display, display : digit, currentOperation: null, lastOperation: this.state.currentOperation });
+            } 
+        } else {
+            this.setState({ display: digit, secondNumber: '', currentOperation: null, lastOperation: '' });
+        }     
     }
 
     validateLength(number) {
@@ -111,12 +119,12 @@ class Main extends Component {
                         <Button onClick={ () => this.handleDigit('3') } value='3'/>
                         <ButtonSpecial onClick={ () => this.handleOperation('+') } value='+'/>                         
                     </Row>
-                    <Row onClick={ () => this.handleDigit('0') } style={{ margin: 0, padding: 0 }}>
-                        <Col md="6" style={{ margin: 0, padding: 5, paddingRight: 0, paddingBottom: 0 }}>
+                    <Row style={{ margin: 0, padding: 0 }}>
+                        <Col onClick={ () => this.handleDigit('0') } md="6" style={{ margin: 0, padding: 5, paddingRight: 0, paddingBottom: 0 }}>
                             <div className="button"> 0 </div>
                         </Col>
                         <Button value='.'/>
-                        <ButtonSpecial onClick={ () => this.setState({ currentOperation: '=' }) } value='='/>                         
+                        <ButtonSpecial onClick={ () => this.handleOperation('=') } value='='/>                         
                     </Row>
                 </Container>
              </div>
